d622a2b2f5ccfcc17a83800f098a8aaa61cf87d1
Lucas Augusto <lucas@primepasscinema.com>
2019-03-11 15:38:58 -0300

    alterado título do App

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import api from '../services/api';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+import api from "../services/api";
 
 export default class Main extends Component {
   static navigationOptions = {
-    title: 'JSHunt',
+    title: "JS Hunt"
   };
 
   state = {
     productsInfo: {},
     docs: [],
-    page: 1,
+    page: 1
   };
 
   componentDidMount() {
@@ -26,21 +32,21 @@ export default class Main extends Component {
       docs: [...this.state.docs, ...docs],
       productInfo,
       page
-    })
+    });
   };
 
   renderItem = ({ item }) => (
-    <View style={ styles.productContainer }>
-      <Text style={ styles.productTitle }>{ item.title }</Text>
-      <Text style={ styles.productDescription }>{ item.description }</Text>
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text style={styles.productDescription}>{item.description}</Text>
 
       <TouchableOpacity
-        style={ styles.productButton }
+        style={styles.productButton}
         onPress={() => {
-          this.props.navigation.navigate('Product', { product: item });
+          this.props.navigation.navigate("Product", { product: item });
         }}
       >
-        <Text style={ styles.productButtonText }>Acessar</Text>
+        <Text style={styles.productButtonText}>Acessar</Text>
       </TouchableOpacity>
     </View>
   );
@@ -57,14 +63,14 @@ export default class Main extends Component {
 
   render() {
     return (
-      <View style={ styles.container }>
+      <View style={styles.container}>
         <FlatList
-          contentContainerStyle={ styles.list }
+          contentContainerStyle={styles.list}
           data={this.state.docs}
-          keyExtractor={ item => item._id }
-          renderItem={ this.renderItem }
-          onEndReached={ this.loadMore }
-          onEndReachedThreshold={ 0.2 }
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.2}
         />
       </View>
     );
@@ -74,43 +80,43 @@ export default class Main extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    backgroundColor: "#fafafa"
   },
   list: {
-    padding: 20,
+    padding: 20
   },
   productContainer: {
-    backgroundColor: '#fafafa',
+    backgroundColor: "#fafafa",
     borderWidth: 1,
-    borderColor: '#ddd',
+    borderColor: "#ddd",
     borderRadius: 5,
     padding: 20,
-    marginBottom: 20,
+    marginBottom: 20
   },
   productTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#333'
+    fontWeight: "bold",
+    color: "#333"
   },
   productDescription: {
     fontSize: 16,
-    color: '#999',
+    color: "#999",
     marginTop: 5,
-    lineHeight: 24,
+    lineHeight: 24
   },
   productButton: {
     height: 42,
     borderRadius: 5,
     borderWidth: 2,
     marginTop: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#DA552F',
-    backgroundColor: 'transparent'
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#DA552F",
+    backgroundColor: "transparent"
   },
   productButtonText: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#DA552F',
-  },
+    fontWeight: "bold",
+    color: "#DA552F"
+  }
 });
